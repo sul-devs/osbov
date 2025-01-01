3db396ef461987af2c6c08cc69c4e25846888227
Sulaiman Kamara <sulaimankamara110@gmail.com>
2025-01-01 23:04:56 +0000

    Setup project with NextJS and Supabase.

---
+++ app/about/page.tsx
@@ -0,0 +1,22 @@
+
+
+export default function About() {
+    return (
+        <>
+        <header id="hero">
+            <div className="container">
+                <h1>About Us <br />
+                Who We Are <br />
+                What We Do</h1>
+            </div>
+        </header>
+        <section id="mission-vision">
+            <div className="container">
+                <h2>Who We Are?</h2>
+                <h3>Our Vision</h3>
+                <h3>Our Mission</h3>
+            </div>
+        </section>
+        </>
+    )
+}
