9f8a4161cb0887fed3db454788c7a60b4d7226ee
Sulaiman Kamara <sulaimankamara110@gmail.com>
2025-01-05 21:38:08 +0000

    Add the navigation bar to the global layout.

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import { Geist } from "next/font/google";
 import { ThemeProvider } from "next-themes";
 import Link from "next/link";
 import "./globals.css";
+import NavigationBar from "@/components/navigation-bar";
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
@@ -37,19 +38,9 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <main className="min-h-screen flex flex-col items-center">
-            <div className="flex-1 w-full flex flex-col gap-20 items-center">
-              <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-                <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
-                  <div className="flex gap-5 items-center font-semibold">
-                    <Link href={"/"}>Next.js Supabase Starter</Link>
-                    <div className="flex items-center gap-2">
-                      <DeployButton />
-                    </div>
-                  </div>
-                  {!hasEnvVars ? <EnvVarWarning /> : <HeaderAuth />}
-                </div>
-              </nav>
+          <main className="min-h-screen flex flex-col">
+            <div className="flex-1 w-full flex flex-col gap-20">
+              <NavigationBar />
               <div className="flex flex-col gap-20 max-w-5xl p-5">
                 {children}
               </div>
