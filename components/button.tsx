f45f74fc0741bf4a5dff8c4db88877a36f5158fa
Sulaiman Kamara <sulaimankamara110@gmail.com>
2025-01-07 23:54:30 +0000

    Create button component.

--- components/button.tsx
+++ components/button.tsx
@@ -1,5 +1,12 @@
-export default function Button({text}:{text: string}) {
-    return (
-        <button className="bg-orange-600 py-3 px-4 rounded hover:bg-orange-700 text-white">{text}</button>
-    )
+import Link from "next/link";
+
+export default function Button({ text, href }: { text: string; href: string }) {
+  return (
+    <Link
+      href={href}
+      className="bg-orange-600 py-3 px-4 rounded hover:bg-orange-700 text-white"
+    >
+      {text}
+    </Link>
+  );
 }
