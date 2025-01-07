e137c4ec7ac27f264a5e25290577453f45b77372
Sulaiman Kamara <sulaimankamara110@gmail.com>
2025-01-07 23:56:04 +0000

    Add StoryCardList to homepage.

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,86 @@
 // import NavigationBar from "@/components/navigation-bar";
-// import Hero from "@/components/hero";
+import Button from "@/components/button";
+import Hero from "@/components/hero";
+import StoryCard from "@/components/story-card";
+import headshot from "@/app/twitter-image.png";
+import Image from "next/image";
+import StoryCardList from "@/components/StoryCardList";
 
 export default async function Home() {
-  return <p>Hello</p>;
+  return (
+    <>
+      <Hero
+        heading={"Home"}
+        paragraph={
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta error fugit repellendus perspiciatis, repudiandae suscipit totam eum necessitatibus ipsum incidunt!"
+        }
+        cta={<Button href={"/about"} text={"Learn more"} />}
+      />
+      <StoryCardList
+        cards={
+          <>
+            <StoryCard
+              headshot={
+                <Image
+                  src={headshot}
+                  width={30}
+                  height={30}
+                  alt="Doctor man."
+                />
+              }
+              order={"default"}
+              summary={"I am doctor man. That's me."}
+              name={"Doctor Man"}
+              cta={<Button text={"Read more"} href={"/contact"} />}
+            />
+            <StoryCard
+              headshot={
+                <Image
+                  src={headshot}
+                  width={30}
+                  height={30}
+                  alt="Doctor man."
+                />
+              }
+              order={"default"}
+              summary={"I am doctor man. That's me."}
+              name={"Doctor Man"}
+              cta={<Button text={"Read more"} href={"/contact"} />}
+            />
+            <StoryCard
+              headshot={
+                <Image
+                  src={headshot}
+                  width={30}
+                  height={30}
+                  alt="Doctor man."
+                />
+              }
+              order={"default"}
+              summary={"I am doctor man. That's me."}
+              name={"Doctor Man"}
+              cta={<Button text={"Read more"} href={"/contact"} />}
+            />
+            <StoryCard
+              headshot={
+                <Image
+                  src={headshot}
+                  width={30}
+                  height={30}
+                  alt="Doctor man."
+                />
+              }
+              order={"default"}
+              summary={"I am doctor man. That's me."}
+              name={"Doctor Man"}
+              cta={<Button text={"Read more"} href={"/contact"} />}
+            />
+          </>
+        }
+        heading={""}
+        description={""}
+        button={<Button text={"hello"} href={"/contact"} />}
+      />
+    </>
+  );
 }
