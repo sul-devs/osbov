2d2637146985e0a1c4aeed822132825082a15335
Sulaiman Kamara <sulaimankamara110@gmail.com>
2025-01-08 14:58:29 +0000

    Fix the layout.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,13 @@
 import DeployButton from "@/components/deploy-button";
 import { EnvVarWarning } from "@/components/env-var-warning";
 import HeaderAuth from "@/components/header-auth";
-import { ThemeSwitcher } from "@/components/theme-switcher";
 import { hasEnvVars } from "@/utils/supabase/check-env-vars";
 import { Geist } from "next/font/google";
 import { ThemeProvider } from "next-themes";
 import Link from "next/link";
 import "./globals.css";
-import NavigationBar from "@/components/navigation-bar";
-import Footer from "@/components/footer";
+import NavigationBar from "@/components/NavigationBar";
+import Footer from "@/components/Footer";
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
