559676f925c3d131c4afde557866fc380bcf55ea
Sulaiman Kamara <sulaimankamara110@gmail.com>
2025-01-08 14:56:56 +0000

    Comment out header.tsx, will soon get deleted.

--- components/header.tsx
+++ components/header.tsx
@@ -1,9 +1,19 @@
-import NavigationBar from "@/components/navigation-bar"
-import Hero from "@/components/hero"
+import NavigationBar from "@/components/NavigationBar";
+import Hero from "@/components/Hero";
 
-export default function Header({heading, description, ctaText, bgImg} : {heading: string, description: string, ctaText: string, bgImg: string}) {
-    <header>
-        <NavigationBar />
-        <Hero heading={heading} description={description} ctaText={ctaText} />
-    </header>
+export default function Header({
+  heading,
+  description,
+  ctaText,
+  bgImg,
+}: {
+  heading: string;
+  description: string;
+  ctaText: string;
+  bgImg: string;
+}) {
+  <header>
+    <NavigationBar />
+    <Hero heading={heading} description={description} ctaText={ctaText} />
+  </header>;
 }
