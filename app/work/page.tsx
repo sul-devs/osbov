ab55ad6404e696b5fbf7e1cbccddb052e3d45c4a
Sulaiman Kamara <sulaimankamara110@gmail.com>
2025-01-09 01:19:10 +0000

    Add hero to page: Work.

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -1,7 +1,16 @@
-
+import Button from "@/components/Button";
+import Hero from "@/components/Hero";
 
 export default function Work() {
-    return (
-        <h1>WORK</h1>
-    )
+  return (
+    <>
+      <Hero
+        heading={"Work"}
+        paragraph={
+          "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe quis similique, nostrum modi exercitationem atque doloribus reiciendis nemo voluptas. Adipisci."
+        }
+        cta={<Button text={"View our Work below."} href={"#"} />}
+      />
+    </>
+  );
 }
