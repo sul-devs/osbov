8fbc310e37c0c94093d6a95741f8585f1a0862a4
Sulaiman Kamara <sulaimankamara110@gmail.com>
2025-01-09 01:16:18 +0000

    Add hero to page: Services.

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,7 +1,16 @@
-
+import Button from "@/components/Button";
+import Hero from "@/components/Hero";
 
 export default function Services() {
-    return (
-        <h1>SERVICES</h1>
-    )
+  return (
+    <>
+      <Hero
+        heading={"Services"}
+        paragraph={
+          "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe quis similique, nostrum modi exercitationem atque doloribus reiciendis nemo voluptas. Adipisci."
+        }
+        cta={<Button text={"View our services below."} href={"#"} />}
+      />
+    </>
+  );
 }
