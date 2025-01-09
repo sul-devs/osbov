76181749aead6c0ba00fa1d43d02d78367354a14
Sulaiman Kamara <sulaimankamara110@gmail.com>
2025-01-09 01:19:35 +0000

    Add hero to page: Testimonials.

--- app/testimonials/page.tsx
+++ app/testimonials/page.tsx
@@ -1,7 +1,16 @@
+import Button from "@/components/Button";
+import Hero from "@/components/Hero";
 
-
-export default function Services() {
-    return (
-        <h1>TESTIMONIALS</h1>
-    )
+export default function Testimonials() {
+  return (
+    <>
+      <Hero
+        heading={"Testimonials"}
+        paragraph={
+          "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe quis similique, nostrum modi exercitationem atque doloribus reiciendis nemo voluptas. Adipisci."
+        }
+        cta={<Button text={"View our Testimonials below."} href={"#"} />}
+      />
+    </>
+  );
 }
