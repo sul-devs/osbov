3aa43fdb3921d37abb20958adfeaf252eb2fa950
Sulaiman Kamara <sulaimankamara110@gmail.com>
2025-01-09 01:20:19 +0000

    Add hero to page: Contact.

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,16 @@
-
+import Button from "@/components/Button";
+import Hero from "@/components/Hero";
 
 export default function Contact() {
-    return (
-        <h1>CONTACT</h1>
-    )
+  return (
+    <>
+      <Hero
+        heading={"Contact"}
+        paragraph={
+          "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe quis similique, nostrum modi exercitationem atque doloribus reiciendis nemo voluptas. Adipisci."
+        }
+        cta={<Button text={"View our Contact below."} href={"#"} />}
+      />
+    </>
+  );
 }
