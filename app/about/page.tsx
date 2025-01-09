4feed4726b8ff53aa8a318044aaa4e5de38b1e93
Sulaiman Kamara <sulaimankamara110@gmail.com>
2025-01-09 01:13:55 +0000

    Add hero to page: About.

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,22 +1,16 @@
-
+import Button from "@/components/Button";
+import Hero from "@/components/Hero";
 
 export default function About() {
-    return (
-        <>
-        <header id="hero">
-            <div className="container">
-                <h1>About Us <br />
-                Who We Are <br />
-                What We Do</h1>
-            </div>
-        </header>
-        <section id="mission-vision">
-            <div className="container">
-                <h2>Who We Are?</h2>
-                <h3>Our Vision</h3>
-                <h3>Our Mission</h3>
-            </div>
-        </section>
-        </>
-    )
+  return (
+    <>
+      <Hero
+        heading={"About"}
+        paragraph={
+          "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe quis similique, nostrum modi exercitationem atque doloribus reiciendis nemo voluptas. Adipisci."
+        }
+        cta={<Button text={"Learn more below"} href={"#"} />}
+      />
+    </>
+  );
 }
